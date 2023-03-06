2c3bb604e530e29ee5a82f40fbf174ba7ae37f62
FahadAijaz <fahad.aijaz4@gmail.com>
2023-03-07 02:22:12 +0500

    exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-
+  let all = good + bad + neutral
   return (
     <div>
       <h1> give feedback</h1>
@@ -22,6 +22,9 @@ const App = () => {
       <Sentiment text={"good"} sentimentValue={good}/>
       <Sentiment text={"neutral"} sentimentValue={neutral}/>
       <Sentiment text={"bad"} sentimentValue={bad}/>
+      <Sentiment text={"all"} sentimentValue={all}/>
+      <Sentiment text={"average"} sentimentValue={(good * 1 + bad * -1)/all }/>
+      <Sentiment text={"positive"} sentimentValue={(good/all) * 100}/>
       
     </div>
   )
