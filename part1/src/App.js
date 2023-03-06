a20df4b16e60ecb41c2a64913b1372687e4208c1
FahadAijaz <fahad.aijaz4@gmail.com>
2023-03-07 00:49:40 +0500

    exercise 1.4

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,5 +1,5 @@
 import Content from "./Content"
-import Total from "./Footer"
+import Total from "./Total"
 import Header from "./Header"
 
 const App = () => {
@@ -10,13 +10,13 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-  const exerciseList = [{ name: part1, exercises: exercises1 },
+  const parts = [{ name: part1, exercises: exercises1 },
   { name: part2, exercises: exercises2 }, { name: part3, exercises: exercises3 }]
   return (
     <div>
       <Header course={course} />
-      <Content exerciseList={exerciseList} />
-      <Total total={exercises1 + exercises2 + exercises3} />
+      <Content exerciseList={parts} />
+      <Total parts={parts} />
     </div>
   )
 }
