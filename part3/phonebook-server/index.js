2926326aef0aa0ed33df5e6cb0a0c4ae3f4642d8
FahadAijaz <fahad.aijaz4@gmail.com>
2023-03-09 00:52:25 +0500

    exercise 3.7

--- part3/phonebook-server/index.js
+++ part3/phonebook-server/index.js
@@ -25,8 +25,19 @@ let persons =
         }
     ]
 
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+app.use(requestLogger)
+
 app.use(cors())
 app.use(express.json())
+
+
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
