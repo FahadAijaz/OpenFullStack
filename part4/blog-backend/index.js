fcdfc8dc39d7848b4ac4d4b55876fd333d74625f
FahadAijaz <fahad.aijaz4@gmail.com>
2023-03-10 01:03:18 +0500

    exercise 4.15

--- part4/blog-backend/index.js
+++ part4/blog-backend/index.js
@@ -1,13 +1,39 @@
+require('dotenv').config()
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const {getAllBlogs, createBlog, Blog, deleteBlog, updateBlog} = require("./mongo");
+const {getAllBlogs, createBlog, Blog, deleteBlog, updateBlog, User} = require("./mongo");
+const jwt = require('jsonwebtoken')
+const loginRouter = require("./controllers/login");
+const usersRouter = require("./controllers/user");
 
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
+const errorHandler = (error, request, response, next) => {
+    logger.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({ error: error.message })
+    } else if (error.name ===  'JsonWebTokenError') {
+        return response.status(400).json({ error: error.message })
+    }
+
+    next(error)
+}
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if (authorization && authorization.startsWith('Bearer ')) {
+        return authorization.replace('Bearer ', '')
+    }
+    return null
+}
 
 app.get('/api/blogs', async (request, response) => {
     const blogs = await getAllBlogs(response);
@@ -17,6 +43,11 @@ app.get('/api/blogs', async (request, response) => {
 
 app.post('/api/blogs', async (request, response) => {
     const blog = new Blog(request.body)
+    const decodedToken = jwt.verify(getTokenFrom(request), process.env.SECRET)
+    if (!decodedToken.id) {
+        return response.status(401).json({ error: 'token invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
     const created = await createBlog(blog);
     if(created){
       response.json(blog)
--- part4/blog-backend/mongo.js
+++ part4/blog-backend/mongo.js
@@ -1,10 +1,36 @@
 const mongoose = require('mongoose')
+const userSchema = new mongoose.Schema({
+    username: String,
+    name: String,
+    passwordHash: String,
+    notes: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Note'
+        }
+    ],
+})
 
+userSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        // the passwordHash should not be revealed
+        delete returnedObject.passwordHash
+    }
+})
+
+const User = mongoose.model('User', userSchema)
 const blogSchema = new mongoose.Schema({
     title: String,
     author: String,
     url: String,
-    likes: Number
+    likes: Number,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
@@ -12,8 +38,8 @@ const Blog = mongoose.model('Blog', blogSchema)
 const mongoUrl = 'mongodb://localhost/bloglist'
 mongoose.connect(mongoUrl)
 
-const getAllBlogs =  async () =>  {
-    return  Blog.find({})
+const getAllBlogs = async () => {
+    return Blog.find({})
 }
 
 const createBlog = async (blog) => {
@@ -26,4 +52,4 @@ const updateBlog = async (blogId, likes) => {
     return Blog.updateOne({_id: blogId}, {likes: likes})
 }
 
-module.exports = {Blog, getAllBlogs, createBlog, deleteBlog, updateBlog}
+module.exports = {Blog, getAllBlogs, createBlog, deleteBlog, updateBlog, User}
