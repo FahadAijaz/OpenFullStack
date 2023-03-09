83124aea3d306fdab05b3eda4ab08ba94e659b73
FahadAijaz <fahad.aijaz4@gmail.com>
2023-03-09 23:50:59 +0500

    exercise 4.8

--- part4/blog-backend/index.js
+++ part4/blog-backend/index.js
@@ -30,3 +30,5 @@ const PORT = 3003
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
+
+module.exports = app
--- part4/blog-backend/test.js
+++ part4/blog-backend/test.js
@@ -1,4 +1,9 @@
 const listHelper = require('./utils/list_helper')
+const {blogs} = require('./utils/initialBlogs')
+const supertest = require('supertest')
+const app = require('./index')
+const api = supertest(app)
+const {Blog} = require('./mongo')
 
 test('dummy returns one', () => {
     const blogs = []
@@ -22,5 +27,51 @@ describe('total likes', () => {
     test('when list has only one blog, equals the likes of that', () => {
         const result = listHelper.totalLikes(listWithOneBlog)
         expect(result).toBe(5)
+    })
+})
+
+describe('test get all blogs', () => {
+    beforeAll(async () => {
+
+        await Blog.insertMany(blogs)
+    })
+    test('blogs are returned as json', async () => {
+        const response = await api
+            .get('/api/blogs')
+        expect(response.status).toBe(200)
+        expect(response.body).toHaveLength(blogs.length)
+
+    })
+    test('persons have ids', async () => {
+        const response = await api
+            .get('/api/blogs')
+        expect(response.status).toBe(200)
+        const blogsObtained = response.body
+        blogsObtained.forEach(j => {
+            expect(j).toHaveProperty('_id')
+        })
+
+
+    })
+    afterAll(async () => {
+        const ids = blogs.map(b => b._id)
+        await Blog.deleteMany({_id: {$in: ids}})
+
+    })
+
+})
+describe('test create blog', () => {
+    const newBlog = {title: "some new blog", author: "some new man", url: "www.google.com", likes: 3}
+    test('new blog is created', async () => {
+        const response = await api
+            .post('/api/blogs')
+            .send(newBlog)
+        expect(response.status).toBe(200)
+
+        const blogFound = await Blog.find({title: "some new blog"})
+        expect(blogFound).toBeDefined()
+    })
+    afterEach(async () => {
+        await Blog.deleteOne({title: newBlog.title})
     })
 })
