017003c93376d4b6cfc01aa4197e6dfa42e0605f
FahadAijaz <fahad.aijaz4@gmail.com>
2023-03-10 18:38:58 +0500

    exercise 4.1

---
+++ part5/bloglist-frontend-main/src/components/Login.js
@@ -0,0 +1,56 @@
+import loginService from '../services/login'
+import { useState } from 'react'
+const Login = () => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+    const [user, setUser] = useState(null)
+    const [errorMessage, setErrorMessage] = useState(null)
+    const handleLogin = async (event) => {
+        event.preventDefault()
+
+        try {
+            const user = await loginService.login({
+                username, password,
+            })
+            setUser(user)
+            setUsername('')
+            setPassword('')
+        } catch (exception) {
+            setErrorMessage('Wrong credentials')
+            setTimeout(() => {
+                setErrorMessage(null)
+            }, 5000)
+        }
+    }
+    const loginForm = () => (
+        <form onSubmit={handleLogin}>
+            <div>
+                username
+                <input
+                    type="text"
+                    value={username}
+                    name="Username"
+                    onChange={({ target }) => setUsername(target.value)}
+                />
+            </div>
+            <div>
+                password
+                <input
+                    type="password"
+                    value={password}
+                    name="Password"
+                    onChange={({ target }) => setPassword(target.value)}
+                />
+            </div>
+            <button type="submit">login</button>
+        </form>
+    )
+    if (user === null) {
+        return (<div>{loginForm()}</div>)
+    }
+    else {
+        return (<div></div>)
+    }
+}
+
+export default Login
